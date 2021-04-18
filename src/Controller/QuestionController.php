0cb903600c6bd3ff99b141bd78561ab3f7fe1256
Ana Carolina <anacarolinaaps@gmail.com>
2021-04-17 20:48:14 -0400

    we are Rocking this Symfony thing

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/", name="question")
+     */
+    public function homepage()
+    {
+        return new Response('What a bewtiching controller we have conjured!!');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf('Future page to show the question "%s"!!', ucwords(str_replace("-"," ", $slug))));
+    }
+}
