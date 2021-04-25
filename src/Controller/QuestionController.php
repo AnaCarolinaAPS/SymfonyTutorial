e406cba841603b62471b759aa81e498904a766a0
Ana Carolina <anacarolinaaps@gmail.com>
2021-04-24 21:19:34 -0400

    Added some css and js to make things awesome

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -9,15 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/", name="question")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('What a bewtiching controller we have conjured!!');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
